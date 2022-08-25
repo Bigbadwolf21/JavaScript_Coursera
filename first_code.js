01916d7e71f717e993c43c33ba5f740f3432da9f
Ayodele Ayomide <66219396+Bigbadwolf21@users.noreply.github.com>
2022-08-25 20:34:08 +0100

    First code

---
+++ first_code.js
@@ -0,0 +1,21 @@
+var petDog = "Rex";
+var petCat= "Pepper";
+// Log my Cat and Dog to the console
+console.log(petDog);
+console.log(petCat);
+
+console.log(`My pet dog's name is: ${petDog}`);
+console.log(`My pet cat's name is: ${petCat}`);
+
+//New Variable declarations
+var catSound = "purr";
+var dogSound = "woof";
+
+//Log the pets name and sounds to the console
+console.log(`${petDog} says ${dogSound}`);
+console.log(`${petCat} says ${catSound}`);
+
+// Reassign the value of catSound to a new value
+catSound = "meow";
+
+console.log(`${petCat} now says ${catSound}`);
